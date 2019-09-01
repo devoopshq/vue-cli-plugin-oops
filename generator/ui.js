0b16ef5b0eb5009d7656cc4bde63a29bab3666b8
daliborgogic <mail@daliborgogic.com>
2019-09-01 21:46:23 +0200

    using prompts, no need for ui

--- generator/ui.js
+++
@@ -1,6 +0,0 @@
-api.describeConfig({
-  id: 'org.ktsn.vue-auto-routing.config',
-  name: 'Greeting configuration',
-  description: 'This config defines the color of the greeting printed',
-  link: 'https://github.com/devoopshq/vue-cli-plugin-oops#readme'
-})
